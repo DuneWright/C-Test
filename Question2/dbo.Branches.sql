2c6f7297be8ce7db0c80cfbc5278d8040a33fb7e
DuneWright <dunealexanderwright96@gmail.com>
2019-12-08 20:28:36 +0200

    delete unneeded files

--- Question2/dbo.Branches.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Branches]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [BranchDesc] VARCHAR(50) NULL, 
-    [u_Id] INT NULL, 
-    CONSTRAINT [FK_Branches_Users] FOREIGN KEY ([u_Id]) REFERENCES [Users]([Id])
-)
--- Question2/dbo.Shifts.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Shifts]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [ShiftDesc] VARCHAR(50) NULL, 
-    [u_Id] NCHAR(10) NOT NULL, 
-    CONSTRAINT [FK_Shifts_Users] FOREIGN KEY ([u_Id]) REFERENCES [Users]([Id])
-)
